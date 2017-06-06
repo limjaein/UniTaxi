63f4b9685323e68cd628d0ab9edcd5b4e074ddf2
jaein <dlawodls96@naver.com>
2017-06-07 06:10:55 +0900

    로그인 회원가입 연결

--- app/src/main/java/com/example/jaein/unitaxi/member.java
+++ app/src/main/java/com/example/jaein/unitaxi/member.java
@@ -5,77 +5,77 @@ package com.example.jaein.unitaxi;
  */
 
 public class member {
-    private String m_id; // 사용자 id
-    private String m_uni; // 대학교
-    private int m_use; // 이용횟수
-    private int m_warn; // 탈주횟수
-    private int m_rel; // 신뢰도
-    private String m_gender; // 성별
-
-    public member(String member_id, String member_uni, String member_gender){
-        this.m_id = member_id;
-        this.m_uni = member_uni;
-        this.m_use = 0;
-        this.m_warn = 0;
-        this.m_rel = 0;
-        this.m_gender = member_gender;
+    private String member_id; // 사용자 id
+    private String member_passwd;
+    private String member_name;
+    private String member_uni; // 대학교
+    private int member_use; // 이용횟수
+    private int member_warn; // 탈주횟수
+    private int member_rel; // 신뢰도
+    private String member_gender; // 성별
+    public member(String member_id, String member_passwd,
+                  String member_uni, String member_name, String member_gender,
+                  int member_use, int member_warn, int member_rel){
+        this.member_id = member_id;
+        this.member_uni = member_uni;
+        this.member_name = member_name;
+        this.member_use = member_use;
+        this.member_passwd = member_passwd;
+        this.member_warn = member_warn;
+        this.member_rel = member_rel;
+        this.member_gender = member_gender;
     }
 
-    public member(String member_id, String member_uni, int member_use,
-                  int member_warn, int member_rel, String member_gender){
-        this.m_id = member_id;
-        this.m_uni = member_uni;
-        this.m_use = member_use;
-        this.m_warn = member_warn;
-        this.m_rel = member_rel;
-        this.m_gender = member_gender;
-    }
     public member(){
-        this.m_id = "";
-        this.m_uni = "";
-        this.m_use = 0;
-        this.m_warn = 0;
-        this.m_rel = 0;
-        this.m_gender = "";
     }
 
     public String getMember_id(){
-        return m_id;
+        return member_id;
     }
     public String getMember_uni(){
-        return m_uni;
+        return member_uni;
+    }
+    public String getMember_passwd() {return member_passwd;}
+    public String getMember_name(){
+        return member_name;
     }
     public int getMember_use(){
-        return m_use;
+        return member_use;
     }
     public int getMember_warn(){
-        return m_warn;
+        return member_warn;
     }
     public int getMember_rel(){
-        return m_rel;
+        return member_rel;
     }
     public String getMember_gender(){
-        return m_gender;
+        return member_gender;
     }
+
+
     public void setMember_id(String member_id){
-        this.m_id = member_id;
+        this.member_id = member_id;
     }
+    public void setMemeber_passwd(String member_passwd){this.member_passwd = member_passwd;}
     public void setMember_uni(String member_uni){
-        this.m_uni = member_uni;
+        this.member_uni = member_uni;
+    }
+    public void setMember_name(String member_name){
+        this.member_name = member_name;
     }
     public void setMember_use(int use){
-        this.m_use = use;
+        this.member_use = use;
         setMember_rel(); // 신뢰도 갱신
     }
     public void setMember_warn(int warn){
-        this.m_warn = warn;
+        this.member_warn = warn;
         setMember_rel(); // 신뢰도 갱신
     }
     public void setMember_rel(){ // 신뢰도 계산
-        if(m_use!=0)
-            m_rel = ((m_use-m_warn)/m_use)*100;
+        if(member_use!=0)
+            member_rel = ((member_use-member_warn)/member_use)*100;
     }
     public void setMember_gender(String member_gender){
-        this.m_gender = member_gender;
+        this.member_gender = member_gender;
     }
 }
--- app/src/main/java/com/example/jaein/unitaxi/u03_Register_Activity.java
+++ app/src/main/java/com/example/jaein/unitaxi/u03_Register_Activity.java
@@ -1,22 +1,124 @@
 package com.example.jaein.unitaxi;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import static com.example.jaein.unitaxi.u02_Login_Activity.db_member;
 
 public class u03_Register_Activity extends AppCompatActivity {
+
+    EditText id,name,passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u03_register);
+
+        init();
     }
 
-    private void goto_Login() {
-        Intent i_Login = new Intent(u03_Register_Activity.this, u02_Login_Activity.class);
-        startActivity(i_Login);
+    private void init() {
+        id = (EditText)findViewById(R.id.JoinId);
+        name = (EditText)findViewById(R.id.JoinName);
+        passwd = (EditText)findViewById(R.id.JoinPass);
     }
 
-    public void RegisterToLogin(View view) { goto_Login(); };
+//    private void goto_Login() {
+//        Intent i_Login = new Intent(u03_Register_Activity.this, u02_Login_Activity.class);
+//        startActivity(i_Login);
+//    }
+    public void checkId(View view){
+        final String str_id = id.getText().toString();
+
+        Query id_query = db_member.orderByChild("member_id").equalTo(str_id);
+        id_query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {//사용가능
+                    Toast.makeText(u03_Register_Activity.this, "사용가능한 아이디입니다", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(u03_Register_Activity.this, "이미 존재하는 아이디입니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void checkName(View view){
+        final String str_name = id.getText().toString();
+
+        Query id_query = db_member.orderByChild("member_name").equalTo(str_name);
+        id_query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {//사용가능
+                    Toast.makeText(u03_Register_Activity.this, "사용가능한 닉네임입니다", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(u03_Register_Activity.this, "이미 존재하는 닉네임입니다", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void RegisterToLogin(View view) {
+        final String str_id = id.getText().toString();
+        final String str_pw = passwd.getText().toString();
+        final String str_name = name.getText().toString();
+
+        Query id_query = db_member.orderByChild("member_id").equalTo(str_id);
+        id_query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue()==null){//사용가능
+                    Query name_query = db_member.orderByChild("member_name").equalTo(str_name);
+                    name_query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.getValue()==null) {//사용가능한 닉네임
+                                member m = new member(str_id,str_pw,"건국대학교",str_name,"w",0,0,0);
+                                db_member.child(str_id).setValue(m);
+
+                                Toast.makeText(getApplicationContext(), "디비저장 완료", Toast.LENGTH_SHORT).show();
+
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(u03_Register_Activity.this, "이미 존재하는 닉네임입니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(u03_Register_Activity.this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
