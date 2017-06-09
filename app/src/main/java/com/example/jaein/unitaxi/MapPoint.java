c19885c57004fa9faadce322902bfebcbf55c8ec
jaein <dlawodls96@naver.com>
2017-06-09 20:45:09 +0900

    맵뷰 합침!

---
+++ app/src/main/java/com/example/jaein/unitaxi/MapPoint.java
@@ -0,0 +1,47 @@
+package com.example.jaein.unitaxi;
+
+/**
+ * Created by DoKyung on 2017. 6. 1..
+ */
+
+public class MapPoint {
+    private String Name;
+    private double latitude;
+    private double longitude;
+
+    public MapPoint(){
+        super();
+    }
+
+    public MapPoint(String Name, double latitude, double longitude) {
+        //super();
+        this.Name = Name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
--- app/src/main/java/com/example/jaein/unitaxi/f01_Fragment.java
+++ app/src/main/java/com/example/jaein/unitaxi/f01_Fragment.java
@@ -1,17 +1,48 @@
 package com.example.jaein.unitaxi;
 
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapGpsManager;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class f01_Fragment extends Fragment {
+public class f01_Fragment extends Fragment implements TMapGpsManager.onLocationChangedCallback {
+
+    private Context mContext = null;
+    private boolean m_bTrackingMode = true;
+
+    private TMapData tmapdata=null;
+    private TMapGpsManager tmapgps = null;
+    private TMapView tmapview = null;
+    private static String mApiKey = "8e36c4a4-66cd-3e71-8f93-06d5754f647a"; // 발급받은 appKey
+    private static int mMarkerID;
+
+    private ArrayList<TMapPoint> m_tmapPoint = new ArrayList<TMapPoint>();
+    private ArrayList<String> mArrayMarkerID = new ArrayList<String>();
+    private ArrayList<MapPoint> m_mapPoint = new ArrayList<MapPoint>();
+
+    private String address;
+    private Double lat = null;
+    private Double lon = null;
 
     public f01_Fragment() {
         // Required empty public constructor
@@ -22,9 +53,92 @@ public class f01_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f01, container, false);
+        View containerView = inflater.inflate(R.layout.fragment_f01, container, false);
+
+        //Tmap 각종 객체 선언
+        tmapdata = new TMapData(); //POI검색, 경로검색 등의 지도데이터를 관리하는 클래스
+        LinearLayout linearLayout = (LinearLayout) containerView.findViewById(R.id.mapview);
+        tmapview = new TMapView(getActivity());
+        linearLayout.addView(tmapview);
+        tmapview.setSKPMapApiKey(mApiKey);
+
+        //addPoint();
+        //showMarkerPoint();
+
+        /* 현재 보는 방향 */
+        tmapview.setCompassMode(true);
+
+        /* 현위치 아이콘표시 */
+        tmapview.setIconVisibility(true);
+
+        /* 줌레벨 */
+        tmapview.setZoomLevel(15);
+        /*지도타입 */
+        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
+        /*언어 설정*/
+        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        tmapgps = new TMapGpsManager(getActivity());//단말기 위치탐색을 위한 클래
+        tmapgps.setMinTime(1000); //위치변경 인식 최소시간설정
+        tmapgps.setMinDistance(5);// 위치변경 인식 최소거리설정
+        //tmapgps.setProvider(tmapgps.NETWORK_PROVIDER); //연결된 인터넷으로 현 위치를 받습니다.
+        //실내일 때 유용합니다.
+        tmapgps.setProvider(tmapgps.GPS_PROVIDER); //gps로 현 위치를 잡습니다.
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //권한이 없을 경우
+            //최초 권한 요청인지, 혹은 사용자에 의한 재요청인지 확인
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // 사용자가 임의로 권한을 취소시킨 경우
+                // 권한 재요청
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            }
+        } else {
+            /// 권한이 있을때만
+            ///////////
+            tmapgps.OpenGps();
+
+            /*  화면중심을 단말의 현재위치로 이동 */
+            tmapview.setTrackingMode(true);
+            tmapview.setSightVisible(true);
+
+            // 풍선에서 우측 버튼 클릭시 할 행동입니다
+            tmapview.setOnCalloutRightButtonClickListener(new TMapView.OnCalloutRightButtonClickCallback() {
+                @Override
+                public void onCalloutRightButton(TMapMarkerItem markerItem) {
+
+                    lat = markerItem.latitude;
+                    lon = markerItem.longitude;
+
+                    //1. 위도, 경도로 주소 검색하기
+                    tmapdata.convertGpsToAddress(lat, lon, new TMapData.ConvertGPSToAddressListenerCallback() {
+                        @Override
+                        public void onConvertToGPSToAddress(String strAddress) {
+                            address = strAddress;
+                        }
+                    });
+
+                    Toast.makeText(getContext(), "주소 : " + address, Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
+        }
+
+        return containerView;
     }
 
     public interface OnFragmentInteractionListener {
+    }
+
+    /// tmap gps listener
+    @Override
+    public void onLocationChange(Location location) {
+        if (m_bTrackingMode) {
+            tmapview.setLocationPoint(location.getLongitude(), location.getLatitude());
+        }
     }
 }
